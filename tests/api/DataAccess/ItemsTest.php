109817e6acb3fdb954eac6f5b64a9719cb8ea993
antoniocordasco <cordasco.a@gmail.com>
2017-10-13 11:38:30 +0200

    completed ItemsTest::testGetItemById()

--- tests/api/DataAccess/ItemsTest.php
+++ tests/api/DataAccess/ItemsTest.php
@@ -12,7 +12,14 @@ class ItemsTest extends \PHPUnit_Framework_TestCase
     {
         $salad = new \Models\Item(1, 'Salad', 7);
         $itemsDataAccess = new \DataAccess\Items();
-
         $this->assertEquals($salad, $itemsDataAccess->getItemById(1));
+
+        $burger = new \Models\Item(2, 'Hamburger', 10);
+        $itemsDataAccess = new \DataAccess\Items();
+        $this->assertEquals($burger, $itemsDataAccess->getItemById(2));
+
+        $chips = new \Models\Item(3, 'Chips', 3);
+        $itemsDataAccess = new \DataAccess\Items();
+        $this->assertEquals($chips, $itemsDataAccess->getItemById(3));
     }
 }
