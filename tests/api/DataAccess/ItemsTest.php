4a47ca54559a0231d04ea51624dc7736849c3096
antoniocordasco <cordasco.a@gmail.com>
2017-10-14 20:32:35 +0100

    added testGetAll(). All the ItemsDAO class should be covered now.

--- tests/api/DataAccess/ItemsTest.php
+++ tests/api/DataAccess/ItemsTest.php
@@ -4,30 +4,46 @@ namespace Tests\Api\DataAccess;
 
 require 'common.php';
 
-use \DataAccess\ItemsDAO as ItemsDAO;
+use DataAccess\ItemsDAO as ItemsDAO;
 
-
+/**
+ * @coversNothing
+ */
 class ItemsTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * Unit test for ItemsDAO::getItemById
+     * Unit test for ItemsDAO::testGetAll.
+     */
+    public function testGetAll()
+    {
+        $return = [];
+        $return[1] = new \Models\Item(1, 'Salad', 7);
+        $return[2] = new \Models\Item(2, 'Hamburger', 10);
+        $return[3] = new \Models\Item(3, 'Chips', 3);
+
+        $itemsDAO = ItemsDAO::getInstance();
+        $this->assertSame($return, $itemsDAO->getAll());
+    }
+
+    /**
+     * Unit test for ItemsDAO::getItemById.
      */
     public function testGetItemById()
     {
         $itemsDAO = ItemsDAO::getInstance();
 
         $salad = new \Models\Item(1, 'Salad', 7);
-        $this->assertEquals($salad, $itemsDAO->getItemById(1));
+        $this->assertSame($salad, $itemsDAO->getItemById(1));
 
         $burger = new \Models\Item(2, 'Hamburger', 10);
-        $this->assertEquals($burger, $itemsDAO->getItemById(2));
+        $this->assertSame($burger, $itemsDAO->getItemById(2));
 
         $chips = new \Models\Item(3, 'Chips', 3);
-        $this->assertEquals($chips, $itemsDAO->getItemById(3));
+        $this->assertSame($chips, $itemsDAO->getItemById(3));
     }
 
     /**
-     * Unit test for ItemsDAO::setOrderedItems and ItemsDAO::getOrderedItems
+     * Unit test for ItemsDAO::setOrderedItems and ItemsDAO::getOrderedItems.
      */
     public function testSetGetOrderedItems()
     {
@@ -38,6 +54,6 @@ class ItemsTest extends \PHPUnit_Framework_TestCase
 
         // data is stored in cookies as JSON, so everything is converted to standard objects.
         // that's why we need to encode and decode if we want to compare the data.
-        $this->assertEquals(json_decode(json_encode($orderedItems)), $itemsDAO->getOrderedItems());
+        $this->assertSame(json_decode(json_encode($orderedItems)), $itemsDAO->getOrderedItems());
     }
 }
