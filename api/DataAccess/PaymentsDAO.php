b6db9fa285b11e6e47334e4e781da4385a19fa1b
antoniocordasco <cordasco.a@gmail.com>
2017-10-15 10:34:12 +0100

    written DAO logic for the saving of a payment and the getting of the total amount paid

---
+++ api/DataAccess/PaymentsDAO.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace DataAccess;
+
+class PaymentsDAO extends BaseDAO
+{
+
+    public function savePayment($amount)
+    {
+        $amountPaid = $this->getTotalAmountPaid();
+        setcookie('paid', ($amountPaid + $amount), 0, '/');
+
+        // we need to also set the variable so that it can be accessed during the same request
+        $_COOKIE['paid'] = ($amountPaid + $amount);
+    }
+
+    public function getTotalAmountPaid()
+    {
+        return (int)$_COOKIE['paid'];
+    }
+}
