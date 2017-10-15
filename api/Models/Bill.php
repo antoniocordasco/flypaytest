a523f5d0261aa13c1c42b211226eda155d2505ba
antoniocordasco <cordasco.a@gmail.com>
2017-10-15 14:23:22 +0100

    aded 2 new fields to the Bill class

--- api/Models/Bill.php
+++ api/Models/Bill.php
@@ -6,15 +6,19 @@ class Bill
 {
     public $total;
     public $toPay;
+    public $closed;
+    public $tip;
 
     /**
      * Bill constructor.
      * @param int $total
      * @param int $toPay
      */
-    public function __construct($total, $toPay)
+    public function __construct($total, $toPay, $tip = 0)
     {
         $this->total = $total;
         $this->toPay = $toPay;
+        $this->closed = ($this->toPay == 0);
+        $this->tip = $tip;
     }
 }
