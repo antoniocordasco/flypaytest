4c68cffb9ef16414f649d8b3b37dfccca78c512f
antoniocordasco <cordasco.a@gmail.com>
2017-10-15 10:34:50 +0100

    written behat test that covers getting a bill when a partial payment has been made

--- features/payments.feature
+++ features/payments.feature
@@ -21,3 +21,14 @@ Feature: payments
               "toPay": 20
           }
             """
+
+  Scenario: Get the bill when a payment has been made
+    Given that "three burgers have been ordered and one of them has been paid"
+    When I request "GET /payments/bill"
+    Then I should get:
+            """
+            {
+              "total": 30,
+              "toPay": 20
+          }
+            """
---
+++ tests/Fixtures/ThreeBurgersHaveBeenOrderedAndOneOfThemHasBeenPaid.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Fixtures;
+
+/**
+ * Defines application features from the specific context.
+ */
+class ThreeBurgersHaveBeenOrderedAndOneOfThemHasBeenPaid
+{
+    /**
+     * Loads the fixture's mocked DAOs
+     */
+    public function load()
+    {
+        $return = [new \Models\Item(2, 'Hamburger', 10),new \Models\Item(2, 'Hamburger', 10),new \Models\Item(2, 'Hamburger', 10)];
+
+        $mock = \Mockery::mock('\DataAccess\ItemsDAO');
+        $mock->shouldReceive('getOrderedItems')->andReturn($return);
+        \DataAccess\ItemsDAO::setInstance($mock);
+
+        $mock = \Mockery::mock('\DataAccess\PaymentsDAO');
+        $mock->shouldReceive('getTotalAmountPaid')->andReturn(10);
+        \DataAccess\PaymentsDAO::setInstance($mock);
+    }
+}
